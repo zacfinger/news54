4cf299be15f37f46a4f5d9cb2bb362a7f8727873
zacfinger <zac@zacfinger.com>
2021-01-18 04:19:25 +0000

    Added TopHeader to display weather. Works on mobile and desktop. Displays dummy data now. Will render live data later

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import TopHeader from './TopHeader'
 import Company_Logo_54News from '../img/Company_Logo_54News.png'
 
 const Header = () => {
 	return (
 		<header id="header" className="navbar fixed-top">
 			<div className="container">
-				<div className="col-5 col-lg-4">
+				<div id="CollapsibleHeader-logo" className="col-5 col-lg-4">
 					<a href="/">
 						<img src={Company_Logo_54News} alt='' />
 					</a>
 				</div>
-				<div className="col-12 col-sm-7 col-lg-8">
+				<div id="HeaderMenu" className="col-7 col-lg-8">
+					<TopHeader />
 				</div>
 			</div>
 		</header>
