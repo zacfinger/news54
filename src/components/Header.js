30e278d232195f990bdabfc0d501f47d8edd0291
zacfinger <zac@zacfinger.com>
2021-01-24 02:19:03 +0000

    Experimental navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,60 @@ import Company_Logo_54News from '../img/Company_Logo_54News.png'
 const Header = () => {
 	return (
 		<header id="header" className="navbar fixed-top">
-			<div className="container">
-				<div id="CollapsibleHeader-logo" className="col-5 col-lg-4">
+			<div className="container-md">
+				<div id="CollapsibleHeader-logo" className="col-4 col-lg-3">
 					<a href="/">
 						<img src={Company_Logo_54News} alt='' />
 					</a>
 				</div>
-				<div id="HeaderMenu" className="col-7 col-lg-8">
+				<div id="HeaderMenu" className="col-8 col-lg-9">
 					<TopHeader />
+					<div className="d-md-none d-lg-block" id="CollapsibleHeader-HeaderMenu">
+						<nav class="navbar navbar-expand-md navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+		{/* potentially remove after this line */}
+		<div class="collapse navbar-collapse">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+		{/* potentially remove before this line */}
+</nav>
+					</div>
+				</div>
+				<div className="col-12 d-lg-none">
+				<nav className="navbar navbar-expand-md navbar-light bg-light">
+                                        <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+                </nav>
+
 				</div>
 			</div>
 		</header>
--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -2,21 +2,23 @@ import React, { Fragment} from 'react'
 
 const TopHeader = () => {
 	return (
-		<div id="CurrentConditionsAtAGlanceDetail">
-		    <div className="CurrentConditionsAtAGlanceDetail-wrapper">
-			<div id="CurrentConditionsAtAGlanceDetail-wrapper-cityName">Tucson</div>
-			<div className="CurrentConditionsAtAGlanceDetail-wrapper-sky d-none d-md-block">Clear</div>
-		    </div>
-		    <div id="CurrentConditionsAtAGlanceDetail-weatherInfo">
-			<span id="CurrentConditionsAtAGlanceDetail-temp">
-				79°
-			</span>
-			<span id="weather-icon" style={{backgroundImage: "url('https://raw.githubusercontent.com/fabianalexisinostroza/Antu-classic/90645fdd27cb53ddd77476dedcf8698488c903a9/Antu/status/64/weather-few-clouds.svg')"}} />
-		    	<div className="CurrentConditionsAtAGlanceDetail-wrapper d-none d-md-inline-block">
-				<div className="CurrentConditionsAtAGlanceDetail-wrapper-sky">H°</div>
-				<div className="CurrentConditionsAtAGlanceDetail-wrapper-sky">L 53°</div>
+		<div id="CollapsibleHeader-topHeader">
+			<div id="CurrentConditionsAtAGlanceDetail">
+		    		<div className="CurrentConditionsAtAGlanceDetail-wrapper">
+				    <div id="CurrentConditionsAtAGlanceDetail-wrapper-cityName">Tucson</div>
+				    <div className="CurrentConditionsAtAGlanceDetail-wrapper-sky d-none d-md-block">Clear</div>
+		    		</div>
+		    		<div id="CurrentConditionsAtAGlanceDetail-weatherInfo">
+				    <span id="CurrentConditionsAtAGlanceDetail-temp">
+					79°
+				    </span>
+				    <span id="weather-icon" style={{backgroundImage: "url('https://raw.githubusercontent.com/fabianalexisinostroza/Antu-classic/90645fdd27cb53ddd77476dedcf8698488c903a9/Antu/status/64/weather-few-clouds.svg')"}} />
+		    		    <div className="CurrentConditionsAtAGlanceDetail-wrapper d-none d-md-inline-block">
+					<div className="CurrentConditionsAtAGlanceDetail-wrapper-sky">H°</div>
+					<div className="CurrentConditionsAtAGlanceDetail-wrapper-sky">L 53°</div>
+				    </div>
+		    		</div>
 			</div>
-		    </div>
 		</div>
 	)
 }
