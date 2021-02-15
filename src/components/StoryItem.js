724f830da29bd5a36ddf0a71bab92a9993520e77
zacfinger <zac@zacfinger.com>
2021-02-15 22:26:57 +0000

    Images now pulled programmatially into frontend

--- src/components/StoryItem.js
+++ src/components/StoryItem.js
@@ -9,12 +9,12 @@ export class StoryItem extends Component {
 	}
 
 	render() {
-		const { guid, title } = this.props.story;
+		const { guid, title, img } = this.props.story;
 		const isLg = this.props.isLg;
 		return (
 			<Link className={'CardContainer-gutterSpacing--sm ' + (isLg ? 'col-8' : 'col-4')} to={`/story/${guid}`}>
 			    <div className={'card bg-dark text-white Card-full-container--' + (isLg ? 'lg' : 'sm')}>
-				<img className="card-img imageContainer" src="https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" />
+				<img className="card-img imageContainer" src={`${img}`} />
 				<div className="p-0 card-img-overlay h-100 d-flex flex-column justify-content-end">
 					<div className="Card-textContainer w-100 p-2">
 						<div className={'Card-full-title--' + (isLg ? 'lg' : 'sm')}>{title}</div>
